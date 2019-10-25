dc9b40057ad83defd958cfad4eb2516fd166b618
VIDYA A P S <35686464+apsvidyA@users.noreply.github.com>
2019-10-25 07:45:17 +0530

    1st ever commit

---
+++ index.php
@@ -0,0 +1,128 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
+<!-- <!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+
+	<h2>Home Page</h2>
+
+  	 //notification message
+  	<?php if (isset($_SESSION['success'])) : ?>
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+  	<?php endif ?>
+
+    // logged in user information
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
+		
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: "Lato", sans-serif;
+}
+
+.sidenav {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+#main {
+  transition: margin-left .5s;
+  padding: 16px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+</style>
+</head>
+<body>
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">Current Statistics</a>
+  <a href="department.php">Departmental Statistics</a>
+  <a href="#">Purchase</a>
+  <a href="#">Sales</a>
+  <a href="#">About</a>
+</div>
+
+<div id="main">
+  
+  <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
+</div>
+
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+</script>
+   
+</body>
+</html> 
---
+++ register.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+	<link rel="stylesheet" href="design2.css">
+	<link href='https://fonts.googleapis.com/css?family=Macondo' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet'>
+</head>
+<body>
+	<div id="registerblock">
+		<h1 id="register">Register</h1>
+
+		<br>
+		
+
+		<form action="register.php" method="POST">
+			<br><br><input type="text" placeholder="Username" id="box" name="username" required>
+			<br><br><input type="email" placeholder="Email" id="box" name="email" required>
+			<br><br><input type="password" placeholder="Password" id="box" name="password" required>
+			<br><br><input type="submit" value="Submit"  name="reg_user" id="Subbox">
+
+		</form>
+	</div>
+</body>
+</html>
+
+<?php
+   include 'connection.php';
+   if (isset($_POST['reg_user'])) {
+   $username = mysqli_real_escape_string($db, $_POST['username']);
+   
+   $email = mysqli_real_escape_string($db, $_POST['email']);
+   $password = mysqli_real_escape_string($db, $_POST['password']); 
+   // Attempt insert query execution
+   $sql = "INSERT INTO user_login (username, email, password) VALUES ('$username', '$email', '$password')";
+   if(mysqli_query($db, $sql)){
+        echo "\nRecords added successfully.";
+        
+   } else{
+       echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
+   }
+   }
+?>
