369fe5c040dd0a40fe3c84ce3eb6eeddad4ceee8
VIDYA A P S <35686464+apsvidyA@users.noreply.github.com>
2019-11-03 20:54:00 +0530

    3rd ever commit

--- pur_delete.php
+++ pur_delete.php
@@ -2,15 +2,22 @@
     include 'connection.php';
     include 'purchase.php';
     
-    $sql = "delete from purchase where purchase_id = $del";
+
+    if(isset($_GET['deleted'])){
+    $sql = "delete from purchase where purchase_id = '{$_GET['did']}'";
     if(mysqli_query($db, $sql)){
-        $con = "delete from stock_details where purchase_id = $del";
+        $con = "delete from stock_details where purchase_id = '{$_GET['did']}'";
         if(mysqli_query($db, $con)){
         echo "\n<script>alert('Records deleted successfully.');</script>";
-        header("location: purchase.php");
-        }
-        
-   } else{
+
+        header('location: purchase.php');
+       }
+       
+    }
+
+    else{
        echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
    }
+    }
+
 ?>
